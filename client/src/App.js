8c30dbc9bd0665e61b6cf09d2113092c3e9ac9f6
fullstackwebguru <fullstackwebguru1214@gmail.com>
2020-01-15 18:30:18 -0500

    update(client): ui fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import SearchForm from './SearchForm';
+import logo from './logo.png';
 
 function Copyright() {
   return (
@@ -14,14 +16,35 @@ function Copyright() {
   );
 }
 
+const useStyles = makeStyles(theme => ({
+  main: {
+    height: `calc(100vh - 190px)`,
+    backgroundColor: theme.palette.background.paper
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  logo: {
+    height: 80
+  }
+}));
+
 export default function App() {
+  const classes = useStyles();
   return (
     <Container maxWidth="sm">
-      <Box my={4}>
+      <Box p={4} className={classes.header}>
+        <img src={logo} alt="Logo" className={classes.logo} />
+      </Box>
+      <Box className={classes.main} pt={4}>
         <Typography variant="h4" component="h1" gutterBottom>
           Search Phone Numbers Database!
         </Typography>
         <SearchForm />
+      </Box>
+      <Box pb={3}>
         <Copyright />
       </Box>
     </Container>
